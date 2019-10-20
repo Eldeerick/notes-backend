bc64a0503671cfd35db20d47c7f0172879f679a0
Elderick <erickoliveiraah@gmail.com>
2019-10-19 21:10:41 -0300

    Adding frontend build

---
+++ build/precache-manifest.b286141bd0f25ce66e663f2153aaca4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d46d10f4dd50cbddc984491a8f967086",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d1bb3abe701fc53fe6a",
+    "url": "/static/css/main.cea62761.chunk.css"
+  },
+  {
+    "revision": "c038632110559987e9a1",
+    "url": "/static/js/2.778eef82.chunk.js"
+  },
+  {
+    "revision": "2d1bb3abe701fc53fe6a",
+    "url": "/static/js/main.4f91be90.chunk.js"
+  },
+  {
+    "revision": "81b527afa2aa250645e1",
+    "url": "/static/js/runtime-main.2cecfcc2.js"
+  }
+]);
