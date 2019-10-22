529ba3ef1a7174e5fa89c2ee08b9436ac1ffcdf6
Erick Oliveira <erickoliveiraah@gmail.com>
2019-10-22 12:35:39 -0300

    Testing commits on linux

--- mongo.js
+++ mongo.js
@@ -20,7 +20,7 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
-const note = new Note({
+const note = new Note ({
   content: 'HTML is Easy',
   date: new Date(),
   important: true,
