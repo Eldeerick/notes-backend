0f335a63f63954b359bd708118620caa0b641270
Elderick <erickoliveiraah@gmail.com>
2020-02-20 21:38:13 -0300

    Refined tests and fixed bugs

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -5,6 +5,7 @@ const app = require('../app')
 const api = supertest(app)
 
 const Note = require('../models/note')
+const User = require('../models/user')
 
 beforeEach(async () => {
   await Note.deleteMany({})
@@ -131,40 +132,76 @@ describe('deletion of a note', () => {
 
     expect(contents).not.toContain(noteToDelete.content)
   })
+
+  test('a note can be deleted', async () => {
+    const notesAtStart = await helper.notesInDb()
+    const noteToDelete = notesAtStart[0]
+  
+    await api
+      .delete(`/api/notes/${noteToDelete.id}`)
+      .expect(204)
+  
+    const notesAtEnd = await helper.notesInDb()
+  
+    expect(notesAtEnd.length).toBe(
+      helper.initialNotes.length - 1
+    )
+  
+    const contents = notesAtEnd.map(r => r.content)
+  
+    expect(contents).not.toContain(noteToDelete.content)
+  })
 })
 
-/*
-test('a specific note can be viewed', async () => {
-  const notesAtStart =  await helper.notesInDb()
+describe('when there is initially one user at db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+    const user = new User({ username: 'root', possword: 'sekret' })
+    await user.save()
+  })
 
-  const noteToView = notesAtStart[0]
+  test('creatin succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
 
-  const resultNote = await api
-    .get(`/api/notes/${noteToView.id}`)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+    const newUser = {
+      username: 'Elderick',
+      name: 'Erick Oliveira',
+      password: 'sekret',
+    }
 
-  expect(resultNote.body.date).toBe(noteToView.date)
-})
-*/
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
 
-test('a note can be deleted', async () => {
-  const notesAtStart = await helper.notesInDb()
-  const noteToDelete = notesAtStart[0]
+      const usersAtEnd = await helper.usersInDb()
+      expect(usersAtEnd.length).toBe(usersAtStart.length + 1)
+  
+      const usernames = usersAtEnd.map(u => u.username)
+      expect(usernames).toContain(newUser.username)
+  })
 
-  await api
-    .delete(`/api/notes/${noteToDelete.id}`)
-    .expect(204)
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await helper.usersInDb()
 
-  const notesAtEnd = await helper.notesInDb()
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen',
+    }
 
-  expect(notesAtEnd.length).toBe(
-    helper.initialNotes.length - 1
-  )
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
 
-  const contents = notesAtEnd.map(r => r.content)
+    expect(result.body.error).toContain('`username` to be unique')
 
-  expect(contents).not.toContain(noteToDelete.content)
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd.length).toBe(usersAtStart.length)
+  })
 })
 
 afterAll(() => {
